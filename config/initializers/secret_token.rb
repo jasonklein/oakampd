cc474a5506f1a10812534cd706abca99d4fdb292
jasonklein <jasonklein@gmail.com>
2014-10-04 20:17:39 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oakampd::Application.config.secret_token = '783d04a16451a8886a34c98a93c5d7e0d94f7f7ced2cae22772987d010c32387662dd8541ad67aefdb0e9458afe4e217537d9a1bddf0c952f8b63221bb80355c'
