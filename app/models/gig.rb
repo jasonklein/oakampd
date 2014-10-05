f83ae005ea4946ffa6cedd514d56b045f04c09b0
jasonklein <jasonklein@gmail.com>
2014-10-05 16:55:41 +0100

    List gigs index view and add default scope to Gig model.

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,14 @@ class Gig < ActiveRecord::Base
   belongs_to :band
   belongs_to :venue
   attr_accessible :price, :showdate, :url, :band_id, :venue_id
+
+  default_scope order("showdate ASC")
+
+  def band_name
+    band.name
+  end
+
+  def venue_details
+    "#{venue.name}—#{venue.street_address}"
+  end
 end
