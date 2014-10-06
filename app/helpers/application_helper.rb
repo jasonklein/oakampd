262cf29c406200a7de78420e8475357bc080c609
jasonklein <jasonklein@gmail.com>
2014-10-06 22:57:48 +0100

    Make header image a link when not on root view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_header_image
+    if params[:controller] == "gigs" && params[:action] == "index"
+      image_tag "oakampd-header-black.png"
+    else
+      link_to image_tag("oakampd-header-black.png"), root_path
+    end
+  end
 end
