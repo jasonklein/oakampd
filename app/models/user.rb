8f032436b293a62442444cdc8d4360cd07a1b5ae
jasonklein <jasonklein@gmail.com>
2014-10-29 15:28:11 +0000

    Add Ability model and add role? function to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def role?(role)
+    self.role.to_s == role.to_s
+  end
 end
