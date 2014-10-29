c9c94bfb690f88b4bc1a6b1ca5b681a74562c97f
jasonklein <jasonklein@gmail.com>
2014-10-29 16:54:25 +0000

    Replace header image; add favicon; revise header styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
   def display_header_image
     if params[:controller] == "gigs" && params[:action] == "index"
-      image_tag "oakampd-header-black.png"
+      image_tag logo_filename
     else
-      link_to image_tag("oakampd-header-black.png"), root_path
+      link_to image_tag(logo_filename), root_path
     end
+  end
+
+  def logo_filename
+    "oak-ampd-logo-bordered.jpg"
+  end
+
+  def favicon_filename
+    "oak-ampd-favicon.ico"
   end
 end
