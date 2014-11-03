68ef791787990d13af23f1824d87440191c1f008
jasonklein <jasonklein@gmail.com>
2014-11-03 22:37:17 +0000

    Proof gigs index view rendering against nil values in gigs.

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -4,29 +4,35 @@ class Gig < ActiveRecord::Base
   default_scope order("showdate ASC")
 
   def display_showdate
-    date = self.showdate
-    month = date.strftime "%B"
-    ### Set display_month to "JUN" for "June" and "OCT" for "October"
-    display_month = month[0..2].upcase
-    day_word = date.strftime "%^a"
-    html = "<div class='gig-month'>#{display_month}</div><div class='day-word'>#{day_word}</div><div class='day-number'>#{date.day}</div>"
+    if self.showdate
+      date = self.showdate
+      month = date.strftime "%B"
+      ### Set display_month to "JUN" for "June" and "OCT" for "October"
+      display_month = month[0..2].upcase
+      day_word = date.strftime "%^a"
+      html = "<div class='gig-month'>#{display_month}</div><div class='day-word'>#{day_word}</div><div class='day-number'>#{date.day}</div>"
+    else
+      html = "<div class='day-word'>TBA</div>"
+    end
     html.html_safe
   end
 
   def band_upcase
-    self.band.upcase
+    self.band ? self.band.upcase : "TBA"
+  end
+
+  def display_venue
+    self.venue_name ? self.venue_name : "TBA"
   end
 
   def display_price
     if self.price && self.price > 0
       '%.2f' % self.price
-    elsif self.price < 0
+    elsif (self.price && self.price < 0) || !self.price
       "TBA"
     else
-      0
+      self.price
     end
   end
-
-
 
 end
