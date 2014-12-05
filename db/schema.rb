6c8db1c633cabeb432a51c11c4f40b528e183bf4
jasonklein <jasonklein@gmail.com>
2014-12-04 20:45:37 -0600

    Add Venue model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141029150840) do
+ActiveRecord::Schema.define(:version => 20141205024156) do
 
   create_table "gigs", :force => true do |t|
     t.date     "showdate"
@@ -42,5 +42,13 @@ ActiveRecord::Schema.define(:version => 20141029150840) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "venues", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "zipcode"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
