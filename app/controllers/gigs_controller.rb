be7190af4f0a4bff8d0a1c9ced4b9783cb4f2460
jasonklein <jasonklein@gmail.com>
2015-02-23 14:39:54 -0600

    Tweak quotation marks.

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -38,7 +38,7 @@ class GigsController < ApplicationController
   def update
     respond_to do |format|
       if @gig.update_attributes(params[:gig])
-        format.html { redirect_to root_path, :notice => 'Gig was successfully updated.' }
+        format.html { redirect_to root_path, :notice => "Gig was successfully updated." }
         format.json { respond_with_bip @gig }
       else
         format.html { render :action => "edit" }
