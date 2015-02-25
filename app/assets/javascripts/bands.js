c57d08b81e461b47a43feda34d0de6ff48eb09ce
jasonklein <jasonklein@gmail.com>
2015-02-24 21:39:38 -0600

    Remove close button and generally style and improve js function for band listings.

--- app/assets/javascripts/bands.js
+++ app/assets/javascripts/bands.js
@@ -4,46 +4,18 @@
 
 var OakAmpdBandsJs = OakAmpdBandsJs || {};
 
-// OakAmpdBandsJs.adjustBandImageDivHeights = function () {
-//   setTimeout(function() {
-//       $(".band-image").each(function(i) {
-//       var image = $(this).children("img")[0];
-//       var ht = $(image).height();
-//       $(this).height(ht);
-//     });
-//   }, 500);
-// }
-
 OakAmpdBandsJs.attachBandListingsClickHandlers = function() {
-  $(".standard").click(function() {
-    var img = $(this).find("img")[0];
-    var thumgImgSrc = $(img).attr("src");
-    var imgParent = $(img).parent();
-    var fullImgSrc = $(imgParent).data("full");
+  $(".band-image").click(function() {
+    var bandListingWrapper = $(this).closest(".band-listing-wrapper");
     var oldProminent = $("#bands-listings").find(".prominent")[0];
-    var oldProminentClose = $(oldProminent).children(".close")[0];
-    var newProminentClose = $(this).children(".close")[0];
 
-    $(img).attr("src", fullImgSrc);
-    $(oldProminentClose).css("display", "none");
-    $(oldProminent).removeClass("prominent").addClass("standard");
-    $(this).removeClass("standard").addClass("prominent");
-    $(newProminentClose).css("display", "initial");
-  });
-};
-
-OakAmpdBandsJs.attachProminentCloseClickHandlers = function() {
-  $(".close").click(function() {
-    var oldProminent = $(this).parent();
-    console.log(oldProminent);
-    $(this).css("display", "none");
-    $(oldProminent).removeClass("prominent").addClass("standard");
+    if (oldProminent) { $(oldProminent).removeClass("prominent").addClass("standard"); }
+    $(bandListingWrapper).removeClass("standard").addClass("prominent");
   });
 };
 
 OakAmpdBandsJs.setup = function() {
   OakAmpdBandsJs.attachBandListingsClickHandlers();
-  OakAmpdBandsJs.attachProminentCloseClickHandlers();
 };
 
 $(OakAmpdBandsJs.setup);
