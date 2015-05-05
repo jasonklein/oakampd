50658152efe1b76f777b5ec3533c5bf481b399a7
jasonklein <jasonklein@gmail.com>
2015-05-05 17:14:43 +0200

    Build out update action.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,5 +7,10 @@ class ContentsController < ApplicationController
   end
 
   def update
+    if @content.update_attributes(params[:content])
+      redirect_to edit_content_path, :notice => "Content was successfully updated."
+    else
+      render "edit"
+    end
   end
 end
